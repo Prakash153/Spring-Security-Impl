932510db3115241bb437319fa20684cb2971b641
Prakash15 <prakashkumarsingh483@gmail.com>
2025-06-29 22:29:31 +0530

    implemented UserDetails for User Entity &  userDetailsSerivce for UserService

---
+++ src/main/java/com/Prakash/springSecurityApplication/entities/User.java
@@ -0,0 +1,38 @@
+package com.Prakash.springSecurityApplication.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "User_Details")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id ;
+    @Column(unique = true)
+    private String email ;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+}
---
+++ src/main/java/com/Prakash/springSecurityApplication/services/UserService.java
@@ -0,0 +1,20 @@
+package com.Prakash.springSecurityApplication.services;
+
+import com.Prakash.springSecurityApplication.exceptions.ResourceNotFoundException;
+import com.Prakash.springSecurityApplication.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("user not found with email: " + username));
+    }
+}
